34b0c6150290b0ea059dcc6e6e00f7804ae7b1d9
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2025-04-30 13:57:52 +0000

    Create index.js

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+// import { makeProgram } from "./boilerplate.js";
+// import { setGeometry } from "./buffers.js";
+
+const vShader = `
+    attribute vec2 a_position;
+    uniform vec2 u_resolution;
+    uniform vec2 u_translation;;
+    void main() {
+        vec2 position = a_position + u_translation;
+        vec2 zeroToOne = position / u_resolution;
+        vec2 zeroToTwo = zeroToOne * 2.0;
+        vec2 clipSpace = zeroToTwo - 1.0;
+        gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+    }
+`;
+const fShader = `
+    precision mediump float;
+    uniform vec4 u_color;
+    void main() {
+        gl_FragColor = u_color;
+    }
+`;
+
+main();
+
+function main() {
+    var canvas = document.querySelector("#c");
+    var gl = canvas.getContext("webgl");
+    if (!gl) {
+        alert("Wah wah, no WebGL for you loser")
+    }
+    // var program = makeProgram
+}
