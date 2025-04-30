e6403d77fad4c9480a6aacac4da0a9382bc380ef
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2025-04-30 14:12:56 +0000

    Create buffers.js

---
+++ scripts/buffers.js
@@ -0,0 +1,20 @@
+function setGeometry(gl) {
+  const rect1 = getRectangle(0, 0, 30, 150);
+  const rect2 = getRectangle(30, 0, 100, 30);
+  const rect3 = getRectangle(30, 60, 67, 90);
+  var positions = rect1.push(...rect2.push(...rect3));
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+};
+
+function getRectangle(x1, y1, x2, y2) {
+  return [
+    x1, y1,
+    x2, y1,
+    x1, y2,
+    x1, y2,
+    x2, y1,
+    x2, y2,
+  ];
+};
+
+export { setGeometry, getRectangle };
