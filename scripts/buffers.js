ee2260f51e023717b05b28d043180d7b54041518
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2025-04-30 16:50:11 +0000

    Update buffers.js

--- scripts/buffers.js
+++ scripts/buffers.js
@@ -2,7 +2,7 @@ function setGeometry(gl) {
   const rect1 = getRectangle(0, 0, 30, 150);
   const rect2 = getRectangle(30, 0, 100, 30);
   const rect3 = getRectangle(30, 60, 67, 90);
-  var positions = rect1.push(...rect2.push(...rect3));
+  var positions = rect1.concat(rect2, rect3);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 };
 
