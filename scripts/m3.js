f1e5231cd0c0076fdf3b8d8cd31376e6c4dae30b
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2025-05-05 16:21:55 +0000

    Update m3.js

--- scripts/m3.js
+++ scripts/m3.js
@@ -1,4 +1,12 @@
 var m3 = {
+  projection: function(width, height) {
+    return [
+      2/width, 0, 0,
+      0, -2/height, 0,
+      -1, 1, 1
+    ];
+  }
+  
   translation: function(tx, ty) {
     return [
       1, 0, 0,
