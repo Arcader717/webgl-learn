ff27e1e8dacd26a39c87e4f7b7bc2ee99d97ad6d
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2025-05-05 15:08:36 +0000

    Create range.js

---
+++ scripts/range.js
@@ -0,0 +1,10 @@
+function setupSlider(id, min, max, step) {
+  const ui = document.querySelector("#ui");
+  const slider = document.querySelector("#" + id);
+  if (slider == null) {
+    throw ("Slider '" + id + "' doesn't exist.")
+  }
+  let 
+  let label = document.createElement("div");
+  let 
+}
