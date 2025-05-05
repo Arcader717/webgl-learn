bfb1e2564422e0cb0c678d1b1c4742633dc52110
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2025-05-05 13:58:02 +0000

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -12,8 +12,8 @@ const vShader = `
     void main() {
         vec2 rotated = vec2(
             a_position.x * u_rotation.y + a_position.y * u_rotation.x,
-            a_position.y * u_rotation.y - a_position.x * u_rotation.x)
-        vec2 position = a_position + u_translation;
+            a_position.y * u_rotation.y - a_position.x * u_rotation.x);
+        vec2 position = rotated + u_translation;
         vec2 zeroToOne = position / u_resolution;
         vec2 zeroToTwo = zeroToOne * 2.0;
         vec2 clipSpace = zeroToTwo - 1.0;
