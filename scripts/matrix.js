9449f2346c17de2adf57b78dd9c6062a861e13f0
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2025-05-12 18:09:06 +0000

    Update matrix.js

--- scripts/matrix.js
+++ scripts/matrix.js
@@ -79,6 +79,18 @@ var m3 = {
 };
 
 var m4 = {
+  perspective: function(fovRads, aspect, near, far) {
+    var f = Math.tan(Math.PI * 0.5 - 0.5 * fovRads);
+    var rangeInv = 1.0 / (near - far);
+
+    return [
+      f / aspect, 0, 0, 0,
+      0, f, 0, 0,
+      0, 0, (near + far) * rangeInv, -1,
+      0, 0, near * far * rangeInv * 2, 0,
+    ];
+  },
+  
   ortho: function(left, right, bottom, top, near, far) {
     return [
       2 / (right-left), 0, 0, 0,
