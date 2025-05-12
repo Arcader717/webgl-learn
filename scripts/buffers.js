5df01ba46b9fb0aaec38899e85701bba2a21a725
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2025-05-12 15:26:20 +0000

    Update buffers.js

--- scripts/buffers.js
+++ scripts/buffers.js
@@ -1,20 +1,78 @@
 function setGeometry(gl) {
-  const rect1 = getRectangle(0, 0, 30, 150);
-  const rect2 = getRectangle(30, 0, 100, 30);
-  const rect3 = getRectangle(30, 60, 67, 90);
-  var positions = rect1.concat(rect2, rect3);
+  const rect3D1 = get3DRectangle([0, 30], [0, 150], [0, 30]);
+  const rect3D2 = get3DRectangle([30, 100], [0, 30], [0,30]);
+  const rect3D3 = get3DRectangle([30, 67], [60, 90], [0, 30]);
+  var positions = rect3D1.concat(rect3D2, rect3D3);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 };
 
 function getRectangle(x1, y1, x2, y2) {
   return [
-    x1, y1, 0,
-    x2, y1, 0,
-    x1, y2, 0,
-    x1, y2, 0,
-    x2, y1, 0,
-    x2, y2, 0,
+    x1, y1,
+    x2, y1,
+    x1, y2,
+    x1, y2,
+    x2, y1,
+    x2, y2,
   ];
 };
 
-export { setGeometry, getRectangle };
+function get3DRectangle(x, y, z) { // first example 
+  var x1 = x[0]; // 0
+  var x2 = x[1]; // 30
+  var y1 = y[0]; // 0
+  var y2 = y[1]; // 150
+  var z1 = z[0]; // 0
+  var z2 = z[1]; // 30
+  return [
+    // front
+    x1, y1, z1,
+    x2, y1, z1,
+    x1, y2, z1,
+    x1, y2, z1,
+    x2, y1, z1,
+    x2, y2, z1,
+
+    // back
+    x1, y1, z2,
+    x2, y1, z2,
+    x1, y2, z2,
+    x1, y2, z2,
+    x2, y1, z2,
+    x2, y2, z2,
+
+    // top,
+    x1, y1, z1,
+    x2, y1, z1,
+    x1, y1, z2,
+    x1, y1, z2,
+    x2, y1, z1,
+    x2, y1, z2,
+
+    // bottom
+    x1, y2, z1,
+    x2, y2, z1,
+    x1, y2, z2,
+    x1, y2, z2,
+    x2, y2, z1,
+    x2, y2, z2,
+
+    // left
+    x1, y1, z1,
+    x1, y2, z1,
+    x1, y1, z2,
+    x1, y1, z2,
+    x1, y2, z1,
+    x1, y2, z2,
+
+    // right
+    x2, y1, z1,
+    x2, y2, z1,
+    x2, y1, z2,
+    x2, y1, z2,
+    x2, y2, z1,
+    x2, y2, z2,
+  ];
+}
+
+export { setGeometry, getRectangle, get3DRectangle };
