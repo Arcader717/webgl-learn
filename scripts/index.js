671e880435b13757667005d04a5ba48a6359ffd5
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2025-05-12 17:11:45 +0000

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -105,8 +105,9 @@ function main() {
     function drawScene() {
         webglUtils.resizeCanvasToDisplaySize(gl.canvas);
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         gl.enable(gl.CULL_FACE);
+        gl.enable(gl.DEPTH_TEST);
         gl.useProgram(program);
         
         gl.enableVertexAttribArray(positionLocation);
