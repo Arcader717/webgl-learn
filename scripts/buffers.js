9360a312ab360abe2abadff820b48467cdd8cbce
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2025-05-12 16:11:54 +0000

    Update buffers.js

--- scripts/buffers.js
+++ scripts/buffers.js
@@ -1,10 +1,78 @@
 function setGeometry(gl) {
+  /* when I want to expand into a more modular system.
+  data = [
+    {"vector": {"x": [0,30], "y": [0,150], "z": [0,30]}, "colors": [front, back, top, bottom, left, right]},
+    {"vector": {"x": [30, 100], "y": [0,30], "z": [0,30]}, "colors": [[R, G, B], [R, G, B], [R, G, B], [R, G, B], [R, G, B], [R, G, B]]},
+    {"vector": {"x": [30, 67], "y": [60, 90], [0, 30]}, "colors": [front},
+  ]
+  */
   const rect3D1 = get3DRectangle([0, 30], [0, 150], [0, 30]);
   const rect3D2 = get3DRectangle([30, 100], [0, 30], [0,30]);
   const rect3D3 = get3DRectangle([30, 67], [60, 90], [0, 30]);
   var positions = rect3D1.concat(rect3D2, rect3D3);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 };
+
+function setColors(gl/*, data*/) {
+  const colors = [[200, 70, 120], [80, 70, 200], [70, 200, 210], [210,100,70], [160, 160, 220], [210, 160, 70]];
+  const color1 = get3DRectColors(colors);
+  const color2 = get3DRectColors(colors);
+  const color3 = get3DRectColors(colors);
+  var colorBuffer = color1.concat(color2, color3);
+  gl.bufferData(gl.ARRAY_BUFFER, new UInt8Array(colorBuffer), gl.STATIC_DRAW);
+}
+
+function get3DRectColors(colors) {
+  var f = color[0]; // front
+  var b = color[1]; // back
+  var t = color[2]; // top
+  var o = color[3]; // bottom
+  var l = color[4]; // left
+  var r = color[5]; // right
+  return [
+    f[0], f[1], f[2],
+    f[0], f[1], f[2],
+    f[0], f[1], f[2],
+    f[0], f[1], f[2],
+    f[0], f[1], f[2],
+    f[0], f[1], f[2],
+
+    b[0], b[1], b[2],
+    b[0], b[1], b[2],
+    b[0], b[1], b[2],
+    b[0], b[1], b[2],
+    b[0], b[1], b[2],
+    b[0], b[1], b[2],
+
+    t[0], t[1], t[2],
+    t[0], t[1], t[2],
+    t[0], t[1], t[2],
+    t[0], t[1], t[2],
+    t[0], t[1], t[2],
+    t[0], t[1], t[2],
+
+    o[0], o[1], o[2],
+    o[0], o[1], o[2],
+    o[0], o[1], o[2],
+    o[0], o[1], o[2],
+    o[0], o[1], o[2],
+    o[0], o[1], o[2],
+
+    l[0], l[1], l[2],
+    l[0], l[1], l[2],
+    l[0], l[1], l[2],
+    l[0], l[1], l[2],
+    l[0], l[1], l[2],
+    l[0], l[1], l[2],
+
+    r[0], r[1], r[2],
+    r[0], r[1], r[2],
+    r[0], r[1], r[2],
+    r[0], r[1], r[2],
+    r[0], r[1], r[2],
+    r[0], r[1], r[2],
+  ];
+}
 
 function getRectangle(x1, y1, x2, y2) {
   return [
@@ -75,4 +143,4 @@ function get3DRectangle(x, y, z) { // first example
   ];
 }
 
-export { setGeometry, getRectangle, get3DRectangle };
+export { setGeometry, getRectangle, get3DRectangle, setColors };
